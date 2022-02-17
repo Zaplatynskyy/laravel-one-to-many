43997e06404eca08eb03b20c8389f06ce2a2459f
Zaplatynskyy <zaplatynskyy@gmail.com>
2022-02-17 18:25:19 +0100

    migration add foregn_key

---
+++ database/migrations/2022_02_17_165634_add-column-foreignkey-in-posts-table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddColumnForeignkeyInPostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            $table->foreignId('category_id')->nullable()->onDelete('set null')->constrained();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            $table->dropForeign(['user_id']);
+            $table->dropColumn('category_id');
+        });
+    }
+}
